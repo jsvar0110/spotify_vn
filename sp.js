c66b68797091cb0ca6cecb37e2a1c49707ca60ed
jsvar0110 <varadjs.r@gmail.com>
2025-01-27 21:03:04 +0530

    cry new commit

--- sp.js
+++ sp.js
@@ -78,7 +78,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -89,8 +89,8 @@ async function displayAlbums() {
     for (let index = 0; index < array.length; index++) {
         const e = array[index];
 
-        if (e.href.includes("/songs/") && !e.href.includes(".htaccess") ) {
-            let folder = e.href.split("/").slice(-2)[1]
+        if (e.href.includes("/songs") && !e.href.includes(".htaccess") ) {
+            let folder = e.href.split("/").slice(-2)[0]
 
             let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json();
